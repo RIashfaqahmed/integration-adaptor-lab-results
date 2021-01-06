f66eadfb4d0bf2dfd0a403af3236142c7a17062a
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2021-01-06 10:12:30 +0000

    NIAD-823: Import project
    - Used Spring Initaliser

---
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/IntegrationAdapterLabResultsApplication.java
@@ -0,0 +1,13 @@
+package uk.nhs.digital.nhsconnect.lab.results;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationAdapterLabResultsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationAdapterLabResultsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/IntegrationAdapterLabResultsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.nhs.digital.nhsconnect.lab.results;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationAdapterLabResultsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
